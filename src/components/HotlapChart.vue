83598037b672bd0139e7977ebd563535b04decf6
Thiago Lira <lira_thiago@msn.com>
2023-05-22 21:27:58 -0300

    Show ideal lap

--- src/components/HotlapChart.vue
+++ src/components/HotlapChart.vue
@@ -1,9 +1,27 @@
 <template>
   <h1>Hotlap</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>S1</th>
+        <th>S2</th>
+        <th>S3</th>
+        <th>Ideal</th>
+      </tr>
+    </thead>
+    <tr>
+      <td v-for="split in state.data.bestSplits" :key="split">
+        {{ split }}
+      </td>
+      <td>
+        {{ idealLap }}
+      </td>
+    </tr>
+  </table>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { reactive, computed } from 'vue';
 import laps from '@/services/laps';
 
 export default {
@@ -19,8 +37,19 @@ export default {
         state.data = data;
       });
 
+    const idealLap = computed(() => {
+      const { data } = state;
+
+      if (data.length === 0) return 0;
+
+      const { bestSplits } = data;
+
+      return bestSplits.reduce((acc, split) => acc + split, 0);
+    });
+
     return {
       state,
+      idealLap,
     };
   },
 };
