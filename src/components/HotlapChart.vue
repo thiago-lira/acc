58a96d203262a3d5edda026e4a18277a503c2c2c
Thiago Lira <lira_thiago@msn.com>
2023-05-22 23:10:02 -0300

    Refactor

--- src/components/HotlapChart.vue
+++ src/components/HotlapChart.vue
@@ -9,6 +9,7 @@
 <script>
 import { reactive, computed } from 'vue';
 import laps from '@/services/laps';
+import timeFormat from '@/utils/time';
 
 import DriverInfo from '@/components/DriverInfo.vue';
 import IdealLap from '@/components/IdealLap.vue';
@@ -42,20 +43,22 @@ export default {
 
       return {
         driverName: `[${shortName}] ${firstName} ${lastName}`,
-        totalTime: timing.totalTime,
+        totalTime: timeFormat.getTime(timing.totalTime),
       };
     });
 
     const idealLapData = computed(() => {
+      const { getTime } = timeFormat;
       const { data } = state;
 
       if (data.length === 0) return 0;
 
       const { bestSplits } = data;
+      const idealLap = getTime(bestSplits.reduce((acc, split) => acc + split, 0));
 
       return {
-        bestSplits,
-        idealLap: bestSplits.reduce((acc, split) => acc + split, 0),
+        bestSplits: bestSplits.map((split) => getTime(split)),
+        idealLap,
       };
     });
 
